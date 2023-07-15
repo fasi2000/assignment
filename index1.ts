eaeef3ead9f8fa1903dc7744ebb40057ec505a92
fasi2000 <139007535+fasi2000@users.noreply.github.com>
2023-07-15 07:01:12 +0500

    Add files via upload

---
+++ index1.ts
@@ -0,0 +1,6 @@
+let myname: string = "faSI";
+console.log ("UPPER CASE"), console.log(myname.toUpperCase());
+console.log ("Lower CASE"), console.log(myname.toLowerCase());
+console.log ("Titlecase"), console.log(myname.toLocaleUpperCase);
+//{function.toLocaleUpperCase(myname)};
+
